5c300176634323ef9366af83ff7968cdb8b8c0ab
aidendoubleday <1301279068@s.tusd1.org>
2026-03-27 15:45:47 +0000

    progress

--- script.js
+++ script.js
@@ -16,9 +16,18 @@ function start() {
 
     let userName = prompt("Enter your name");
     console.log(userName)
+    addName(userName,lastname);
 
-   
+   let userLastname = prompt("Enter your last name");
 }
- function addName(newName){
 
+
+function addName(newName,lastname){
+    let heading2 = document.createElement("h2");
+
+    heading2.innerText = newName;
+    heading2.style.color = "blue";
+    heading2.style.textDecoration = "underline";
+
+    document.body.appendChild(heading2);
 }
